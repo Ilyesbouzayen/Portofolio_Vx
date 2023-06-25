7972432fc0b75b548c7b9f4499781620cf433f18
Ilyes Bouzayen <114899410+Elijah099@users.noreply.github.com>
2023-06-25 16:34:04 +0100

    First Commit

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,84 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            I'm a versatile full stack developer and UI/UX designer with motion graphic design skills. Proficient in HTML, CSS, JavaScript, and modern frameworks like React and Angular. I create intuitive and visually stunning interfaces that prioritize user experience. With a keen eye for aesthetics, I design engaging experiences that align with brand identities. Additionally, I bring designs to life through captivating animations and effects using tools like Adobe After Effects and Cinema 4D. I'm passionate about combining development, design, and motion graphics to deliver exceptional digital solutions. Let's collaborate to create impactful experiences that leave a lasting impression.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "Higher Technician's Certificate in Multimedia and Digital Marketing",
+      content: (
+        <>
+          <p>
+            Start Date : 2022 - End Date: Present <br />
+            Training Center : Media Formation
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "Bachelor's Degree in Electrical Engineering - Embedded Systems (ES) Track",
+      content: (
+        <>
+          <p>
+            Start Date : 2019 - End Date: Present <br />
+            University :Higher Institute of Informatics and Mathematics of Monastir
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "Technical high school diploma",
+      content: (
+        <>
+          <p>
+            Start Date : 2018 - End Date: 2019 <br />
+            School :Second High School of Enfidha 1
+          </p>
+        </>
+      ),
+    },
+    
+  ],
+  3: [
+    {
+      title: "2022 Front End Developer",
+      content: (
+        <>
+          <p>
+            In my front-end developer experience, I specialize in creating interactive and responsive user interfaces. I utilize HTML, CSS, and JavaScript, along with modern frameworks, to develop visually appealing and functional websites, focusing on delivering seamless user experiences across different devices and browsers.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2020 UX Designer",
+      content: (
+        <>
+          <p>
+            n my UI/UX design experience, I craft intuitive and visually appealing interfaces that prioritize user experience, utilizing design principles, wireframing, prototyping, and user research to create seamless and engaging digital experiences.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2018 Graphic Designer ",
+      content: (
+        <>
+          <p>
+            In graphic design, I create captivating visuals that effectively communicate messages and enhance brand identities.
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
